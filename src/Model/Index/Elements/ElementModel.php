363fb5c0e866903ea98492ff694600cf7444250b
Irmantas Puodžiukas <irmantas.puodziukas@gmail.com>
2022-09-12 02:10:37 +0300

    Define basic nodes of Element model

--- src/Model/Index/Elements/ElementModel.php
+++ src/Model/Index/Elements/ElementModel.php
@@ -9,5 +9,79 @@ use JMS\Serializer\Annotation as Serializer;
  */
 class ElementModel
 {
+    /**
+     * @var string
+     *
+     * @Serializer\Type("string")
+     * @Serializer\XmlAttribute()
+     */
+    public string $name;
 
+    /**
+     * @var string
+     *
+     * @Serializer\Type("string")
+     * @Serializer\XmlAttribute()
+     */
+    public string $type;
+
+    /**
+     * @var string
+     *
+     * @Serializer\Type("string")
+     * @Serializer\XmlAttribute()
+     */
+    public string $resource;
+
+    /**
+     * @var string
+     *
+     * @Serializer\Type("string")
+     * @Serializer\XmlAttribute()
+     */
+    public string $id;
+
+    /**
+     * @var string
+     *
+     * @Serializer\Type("string")
+     */
+    public string $supports;
+
+    /**
+     * @var string
+     *
+     * @Serializer\Type("HTML")
+     */
+    public string $description;
+
+    public \stdClass $sheet;
+
+    public \stdClass $rules;
+
+    /**
+     * @var string
+     *
+     * @Serializer\Type("string")
+     */
+    public string $requirements;
+
+    public \stdClass $setters;
+
+    /**
+     * @var string
+     *
+     * @Serializer\Type("string")
+     */
+    public string $prerequisite;
+
+    public \stdClass $compendium;
+
+    public \stdClass $spellcasting;
+
+    public \stdClass $multiclass;
+
+    public \stdClass $extract;
+
+    public \stdClass $setter;
 }
