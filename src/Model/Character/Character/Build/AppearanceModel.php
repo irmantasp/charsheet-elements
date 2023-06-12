4e857987803344bba97fd7e26fe9cee86dabf167
Irmantas Puodžiukas <irmantas.puodziukas@gmail.com>
2023-06-12 23:13:53 +0300

    #4 Add appearance model

--- src/Model/Character/Character/Build/AppearanceModel.php
+++ src/Model/Character/Character/Build/AppearanceModel.php
@@ -10,4 +10,59 @@ use JMS\Serializer\Annotation as Serializer;
 class AppearanceModel
 {
 
+    /**
+     * @var string
+     *
+     * @Serializer\Type("string")
+     * @Serializer\SerializedName("portrait")
+     */
+    public string $portrait;
+
+    /**
+     * @var int
+     *
+     * @Serializer\Type("int")
+     * @Serializer\SerializedName("age")
+     */
+    public int $age;
+
+    /**
+     * @var string
+     *
+     * @Serializer\Type("string")
+     * @Serializer\SerializedName("height")
+     */
+    public string $height;
+
+    /**
+     * @var string
+     *
+     * @Serializer\Type("string")
+     * @Serializer\SerializedName("weight")
+     */
+    public string $weight;
+
+    /**
+     * @var string
+     *
+     * @Serializer\Type("string")
+     * @Serializer\SerializedName("eyes")
+     */
+    public string $eyes;
+
+    /**
+     * @var string
+     *
+     * @Serializer\Type("string")
+     * @Serializer\SerializedName("skin")
+     */
+    public string $skin;
+
+    /**
+     * @var string
+     *
+     * @Serializer\Type("string")
+     * @Serializer\SerializedName("hair")
+     */
+    public string $hair;
 }
