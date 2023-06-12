e9dd4064b9540e5cc7d84f4e2f4df3ad282ff6a1
Irmantas Puodžiukas <irmantas.puodziukas@gmail.com>
2023-06-12 23:25:15 +0300

    #4 Add abilities

--- src/Model/Character/Character/Build/AbilitiesModel.php
+++ src/Model/Character/Character/Build/AbilitiesModel.php
@@ -9,5 +9,62 @@ use JMS\Serializer\Annotation as Serializer;
  */
 class AbilitiesModel
 {
+    /**
+     * @var int|null
+     *
+     * @Serializer\Type("integer")
+     * @Serializer\SerializedName("available-points")
+     * @Serializer\XmlAttribute()
+     * @Serializer\SkipWhenEmpty()
+     */
+    public ?int $availablePoints;
+
+    /**
+     * @var int
+     *
+     * @Serializer\Type("integer")
+     * @Serializer\SerializedName("strength")
+     */
+    public int $strength;
+
+    /**
+     * @var int
+     *
+     * @Serializer\Type("integer")
+     * @Serializer\SerializedName("dexterity")
+     */
+    public int $dexterity;
+
+    /**
+     * @var int
+     *
+     * @Serializer\Type("integer")
+     * @Serializer\SerializedName("constitution")
+     */
+    public int $constitution;
+
+    /**
+     * @var int
+     *
+     * @Serializer\Type("integer")
+     * @Serializer\SerializedName("intelligence")
+     */
+    public int $intelligence;
+
+    /**
+     * @var int
+     *
+     * @Serializer\Type("integer")
+     * @Serializer\SerializedName("wisdom")
+     */
+    public int $wisdom;
+
+    /**
+     * @var int
+     *
+     * @Serializer\Type("integer")
+     * @Serializer\SerializedName("charisma")
+     */
+    public int $charisma;
 
 }
